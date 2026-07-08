03a7138413972a575ef8ec9866c5c4448f58d6db
Marcos Santos <mc_santos93@yahoo.com>
2026-07-07 21:47:32 -0400

    Adding the index to for loop print

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 print("Hello, Git World!")
 word = "Masterclass"
-for char in word:
-    print(char)
+for index, char in enumerate(word):
+    print(index, char)
 
 ## Code created by: Marcos Santos ##
 ## Updated July 7th, 2026 ##
